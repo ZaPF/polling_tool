fafcd438430ff8557440c3639050c16fd7e67165
Timo S. Prinz <t.prinz@tu-berlin.de>
2021-05-10 21:03:46 +0200

    Remove app.views import

--- app/urls.py
+++ app/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from app.views import index
 
 urlpatterns = [
     path('', include('polls.urls', namespace="polls")),
