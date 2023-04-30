d4f2145d5d4263735bbddab8fbab19f976a3edef
Timo S. Prinz <t.prinz@campus.tu-berlin.de>
2023-04-30 16:27:10 +0200

    Removed Uni support for SoSe23, migrations broken

--- polls/models.py
+++ polls/models.py
@@ -16,15 +16,14 @@ class Choice(models.Model):
     def __str__(self):
         return self.choice_text
 
-class Uni(models.Model):
-    uni_id = models.IntegerField(unique=True, primary_key=True)
-    uni_name = models.CharField(max_length=200)
+class User(models.Model):
+    user_name = models.CharField(max_length=200, unique=True, primary_key=True)
     voting_rights = models.BooleanField(default=True)
     
     def __str__(self):
-        return self.uni_name
+        return self.user_name
 
 class Vote(models.Model):
     choice = models.ForeignKey(Choice, on_delete=models.CASCADE, related_name='votes')
-    uni = models.ForeignKey(Uni, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=User.objects.get_or_create(user_name="Migrated, delete me"))
     
--- polls/views.py
+++ polls/views.py
@@ -3,27 +3,22 @@ from django.urls import reverse
 
 from django.http import HttpResponseRedirect
 
-from .models import Question, Choice, Vote, Uni
+from .models import Question, Choice, Vote, User
 
 def get_message(request):
-    if request.session.get('uni') is None:
-        if request.session.get('user') is None:
+    if request.session.get('user') is None:
             msg = "Hallo. Du bist nicht eingeloggt und daher nicht stimmberechtigt. Melde dich im ZaPF-Auth an, falls du abstimmen können möchtest."
-        else:
-            msg = f"Hallo {request.session['user']['firstName']}, du bist nicht für die ZaPF angemeldet und daher nicht stimmberechtigt."
-    elif not request.session['confirmed']:
-        msg = f"Hallo {request.session['user']['firstName']}, deine Fachschaft hat dich nicht bestätigt und du bist daher nicht stimmberechtigt."
     else:
-        uni, created = Uni.objects.get_or_create(uni_id=request.session['uni'], defaults={'uni_name': request.session['unis'][str(request.session['uni'])]},)
-        if uni.voting_rights:
-            msg = f"Hallo {request.session['user']['firstName']}, du bist für {request.session['unis'][str(request.session['uni'])]} wahlberechtigt."
+        user, created = User.objects.get_or_create(user_name=request.session.get('user'))
+        if user.voting_rights:
+            msg = f"Hallo {request.session['user']['firstName']}, du bist stimmberechtigt."
         else:
-            msg = f"Hallo {request.session['user']['firstName']}, du bist als Teil einer nicht stimmberechtigten Teilnehmikagruppe angemeldet und daher nicht stimmberechtigt."
+            msg = f"Hallo {request.session['user']['firstName']}, du bist nicht stimmberechtigt, weil jemand deinen Account als nicht stimmberechtigt markiert hat."
     return msg
 
 def results(request):
-    if request.session.get('uni') is not None:
-        uni, created = Uni.objects.get_or_create(uni_id=request.session['uni'], defaults={'uni_name': request.session['unis'][str(request.session['uni'])]},)
+    if request.session.get('user') is not None:
+        user, created = User.objects.get_or_create(user_name=request.session.get('user'))
 
     current_questions = [question for question in Question.objects.all() if question.active]
     questions = [question for question in Question.objects.all() if not question.active]
@@ -39,20 +34,19 @@ def results(request):
     return render(request, 'polls/results.html', context)
 
 def open_polls(request):
-    if request.session.get('uni') is not None:
-        uni, created = Uni.objects.get_or_create(uni_id=request.session['uni'], defaults={'uni_name': request.session['unis'][str(request.session['uni'])]},)
+    if request.session.get('user') is not None:
+        user, created = User.objects.get_or_create(user_name=request.session.get('user'))
 
     questions = [question for question in Question.objects.all() if question.active]
     context = {
         'message': get_message(request),
         'questions': questions,
-        'unis': request.session['unis'],
     }
     return render(request, 'polls/open_polls.html', context)
 
 def vote(request, question_id):
-    if request.session.get('uni') is not None:
-        uni, created = Uni.objects.get_or_create(uni_id=request.session['uni'], defaults={'uni_name': request.session['unis'][str(request.session['uni'])]},)
+    if request.session.get('user') is not None:
+        user, created = User.objects.get_or_create(user_name=request.session.get('user'))
 
     question = get_object_or_404(Question, pk=question_id)
     
@@ -60,46 +54,42 @@ def vote(request, question_id):
         context = {
             'message': get_message(request),
             'question': question,
-            'unis': request.session['unis'],
             'error_message': "Diese Abstimmung ist geschlossen.",
         }
         print(context)
         # Redisplay the question voting form.
         return render(request, 'polls/vote.html', context)
 
-    if not request.session['confirmed']:
+    if not user.voting_rights:
         context = {
             'message': get_message(request),
             'question': question,
-            'unis': request.session['unis'],
             'error_message': "Du bist nicht stimmberechtigt.",
         }
         print(context)
         # Redisplay the question voting form.
         return render(request, 'polls/vote.html', context)
     
-    uni = get_object_or_404(Uni, uni_id=request.session['uni'])
     try:
         selected_choice = question.choices.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         context = {
             'message': get_message(request),
             'question': question,
-            'unis': request.session['unis'],
-            'error_message': "You didn't select a choice.",
+            'error_message': "You didn't select a valid choice.",
         }
         
         # Redisplay the question voting form.
         return render(request, 'polls/vote.html', context)
     else:
-        votes = Vote.objects.filter(choice__question = question, uni = uni)
+        votes = Vote.objects.filter(choice__question = question, user = user)
         if len(votes) > 1:
             raise Exception("Multiple Votes detected")
         elif len(votes) == 1:
             new_vote = votes[0]
             new_vote.choice = selected_choice
         else:
-            new_vote, created = votes.get_or_create(uni=uni, defaults={'choice': selected_choice})   
+            new_vote, created = votes.get_or_create(user=user, defaults={'choice': selected_choice})   
         new_vote.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
